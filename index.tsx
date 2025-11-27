f7db2dae2816aa7d9f52279531a7582d4de73724
kauanofraga <kauanofraga@gmail.com>
2025-11-27 03:06:29 +0000

    fix: Adiciona AppProvider para corrigir erro de renderização

--- index.tsx
+++ index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {AppProvider} from '@shopify/polaris';
+import enTranslations from '@shopify/polaris/locales/en.json';
 import App from './App';
+import '@shopify/polaris/build/esm/styles.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -10,6 +13,8 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <AppProvider i18n={enTranslations}>
+      <App />
+    </AppProvider>
   </React.StrictMode>
 );
